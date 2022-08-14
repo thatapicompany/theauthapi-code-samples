0979ad77a313380fa7e99ea069ca85f20323841a
Nick W Forsberg <456363+seventy6@users.noreply.github.com>
2022-08-14 15:13:07 +0100

    Update validateAxios.js

--- js/axios/validateAxios.js
+++ js/axios/validateAxios.js
@@ -2,12 +2,12 @@ const axios = require("axios");
 require("dotenv").config();
 
 const accessKey =
-  "live_access_tsUTXAb38F59y0uT5vXYh30ceVXSgHuyuxBjPWOqe3Gamu60bqPQ1Vrud7OoNo6Q"; //process.env.ACCESS_TOKEN;
+  process.env.ACCESS_TOKEN;
 
 async function authApiKey(apiKey) {
   try {
     const { data } = await axios.get(
-      "https://staging-api.theauthapi.com/api-keys/auth/" + apiKey,
+      "https://api.theauthapi.com/api-keys/auth/" + apiKey,
       {
         headers: {
           ContentType: "application/json",
@@ -23,7 +23,7 @@ async function authApiKey(apiKey) {
 
 (async () => {
   const validateKey = await authApiKey(
-    "live_3haYyNgeZ6ISfGOnIDlIBlAsXLg1cA8QhVcJsQF2CHsPqwaPgLPfxcQ2Lvs5AtHE"
+    process.env.TEST_KEY
   ); //process.env.TEST_KEY);
   console.log(validateKey);
 })();
