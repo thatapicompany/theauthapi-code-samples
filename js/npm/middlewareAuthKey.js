46d8a2a688b5f37b02881a5fdb27baec1d1dc9d4
Nick W Forsberg <456363+seventy6@users.noreply.github.com>
2022-08-14 15:14:32 +0100

    Update middlewareAuthKey.js

--- js/npm/middlewareAuthKey.js
+++ js/npm/middlewareAuthKey.js
@@ -4,8 +4,7 @@ const app = express();
 const TheAuthAPI = require("theauthapi").default; //for older setups
 
 const theAuthAPI = new TheAuthAPI(
-  "live_access_tsUTXAb38F59y0uT5vXYh30ceVXSgHuyuxBjPWOqe3Gamu60bqPQ1Vrud7OoNo6Q",
-  { host: "https://staging-api.theauthapi.com/" }
+  process.env.ACCESS_TOKEN
 );
 
 app.use(function (req, res, next) {
