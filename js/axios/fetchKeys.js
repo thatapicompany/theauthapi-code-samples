5fcbfb10b4beb8f8d943e3c6cdfb462c3fdf1092
Nick W Forsberg <456363+seventy6@users.noreply.github.com>
2022-08-15 11:29:44 +0100

    Create fetchKeys.js

---
+++ js/axios/fetchKeys.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const accessKey = process.env.ACCESS_TOKEN;
+
+async function featchAllApiKeys() {
+  try {
+    const { data } = await axios.get(
+      "https://staging-api.theauthapi.com/api-keys",
+      {
+        headers: {
+          ContentType: "application/json",
+          "x-api-key": accessKey,
+        },
+      }
+    );
+    return console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+(async () => {
+  const allKeys = await featchAllApiKeys();
+  console.log(allKeys);
+})();
