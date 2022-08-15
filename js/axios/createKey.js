4538a84a352f4a40d3214a47bf74d209bdadced0
Nick W Forsberg <456363+seventy6@users.noreply.github.com>
2022-08-15 12:02:12 +0100

    Imporoved the createkey example

--- js/axios/createKey.js
+++ js/axios/createKey.js
@@ -1,31 +1,45 @@
 import axios from "axios";
+import "dotenv/config";
 
 const accessKey = process.env.ACCESS_TOKEN;
 
 async function createApiKey(apiKey) {
-    try {
-        const { data } = await axios.post(
-            'https://api.theauthapi.com/api-keys',
-            apiKey,
-            {
-                headers: {
-                    ContentType: "application/json",
-                    "x-api-key": accessKey,
-                },
-            }
-        );
-        return data;
-    } catch (error) {
-        // handle error
-    }
+  try {
+    return axios
+      .post("https://staging-api.theauthapi.com/api-keys", apiKey, {
+        headers: {
+          ContentType: "application/json",
+          "x-api-key": accessKey,
+        },
+      })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    // handle error
+  }
 }
 
 const myKey = {
-    name: "Node sample API key",
-    projectId: process.env.PROJECT_ID,
+  name: "API-KEY-NAME",
+  customUserId: "nick@theauthapi.com",
+  customAccountId: "123",
+  key: "KEY-UID-" + Math.random(),
+  customMetaData: {
+    featureFlagOne: true,
+    featureFlagTwo: false,
+  },
+  expiry: "2022-09-01 00:00:00", // optional expiry date
+  rateLimitConfigs: {
+    rateLimit: 600,
+    rateLimitTtl: 60,
+  },
 };
 
 (async () => {
-    const createdKey = await createApiKey(myKey);
-    console.log(createdKey);
+  const createdKey = await createApiKey(myKey);
+  console.log(createdKey.data, createdKey.headers);
 })();
