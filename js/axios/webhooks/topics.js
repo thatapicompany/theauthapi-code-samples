34bb4e378af159244f22090d230ef53e12c05c08
Nick W Forsberg <456363+seventy6@users.noreply.github.com>
2022-08-29 17:07:26 +0100

    Added fetch topics example

---
+++ js/axios/webhooks/topics.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import "dotenv/config";
+
+const accessKey = process.env.ACCESS_TOKEN;
+const apiUrl = process.env.production
+  ? "https://api.theauthapi.com"
+  : process.env.TESTING_URL;
+async function getTopics(webhookData) {
+  try {
+    return axios
+      .get(apiUrl + "/webhooks/topics", {
+        headers: {
+          ContentType: "application/json",
+          "x-api-key": accessKey,
+        },
+      })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    // handle error
+  }
+}
+//fetch all webhook topics
+(async () => {
+  const topicsData = await getTopics();
+  console.log(topicsData.data);
+})();
