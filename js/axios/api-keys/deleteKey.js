8adb2ae0f18e6ecb31fa242cba4b8a5ae5185537
Fibi <charafbahria@gmail.com>
2022-09-08 23:49:07 +0100

    [axios/deletedKey] use async/await, return proper data

--- js/axios/api-keys/deleteKey.js
+++ js/axios/api-keys/deleteKey.js
@@ -5,27 +5,26 @@ const accessKey = process.env.ACCESS_TOKEN;
 const apiUrl = process.env.production
   ? "https://api.theauthapi.com"
   : process.env.TESTING_URL;
+
 async function deleteKey(apiKey) {
   try {
-    return axios
+    const { data } = await axios
       .delete(apiUrl + "/api-keys/" + apiKey, {
         headers: {
           ContentType: "application/json",
           "x-api-key": accessKey,
         },
       })
-      .then(function (response) {
-        return response;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    return data;
   } catch (error) {
     // handle error
+    if (error.response) {
+      console.log(error.response.data);
+    }
   }
 }
 
 (async () => {
-  const deleteKeyResults = await deleteKey(process.env.TEST_KEY);
-  console.log(deleteKeyResults.data, deleteKeyResults.headers);
+  const deleted = await deleteKey(process.env.TEST_KEY);
+  console.log('key has been deleted?', deleted);
 })();
