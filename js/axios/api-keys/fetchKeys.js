209d899dbf3ba999ee354e87fc9e9c2b12b4fdfe
Fibi <charafbahria@gmail.com>
2022-09-09 00:05:33 +0100

    [axios/fetchKeys] add more examples

--- js/axios/api-keys/fetchKeys.js
+++ js/axios/api-keys/fetchKeys.js
@@ -5,11 +5,12 @@ const accessKey = process.env.ACCESS_TOKEN;
 const apiUrl = process.env.production
   ? "https://api.theauthapi.com"
   : process.env.TESTING_URL;
+const projectId = process.env.PROJECT_ID;
 
 async function fetchApiKeys(filters) {
   try {
     const {data} = await axios
-      .get(`${apiUrl}/api-keys?${filters}`, {
+      .get(`${apiUrl}/api-keys${filters ? `?${filters}` : ''}`, {
         headers: {
           ContentType: "application/json",
           "x-api-key": accessKey,
@@ -24,6 +25,12 @@ async function fetchApiKeys(filters) {
     }
   }
 }
+
+function getFiltersQuery(filters) {
+  return Object.entries(filters).map(([key, value]) => `${key}=${value}`)
+      .join('&');
+}
+
 const filters = "";
 //const filters = "?name=sdfsdfsdfsdf&isActive=false";
 //const filters = "?isActive=false";
@@ -31,6 +38,20 @@ const filters = "";
 //const filters = "?customUserId=VALUE";
 
 (async () => {
+  // get all keys
   const apiKeys = await fetchApiKeys(filters);
-  console.log(apiKeys);
+  console.log('All Keys', apiKeys);
+
+  // get keys with a specific name
+  const nameFilteredKeys = await fetchApiKeys(getFiltersQuery({
+    name: "new api key",
+  }));
+  console.log('Keys filtered using name', nameFilteredKeys);
+
+  // get keys with a specific name
+  const projectCustomUserIdKeys = await fetchApiKeys(getFiltersQuery({
+    projectId,
+    customUserId: null,
+  }));
+  console.log('Keys filtered using projectId and customUserId', projectCustomUserIdKeys);
 })();
