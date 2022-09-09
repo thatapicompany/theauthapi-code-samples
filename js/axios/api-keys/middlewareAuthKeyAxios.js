5a55c66083615b3d68b57c1055dd52e41bc34411
Fibi <charafbahria@gmail.com>
2022-09-09 17:08:59 +0100

    [axios/middlewareAuthKeyAxios] use async/await

--- js/axios/api-keys/middlewareAuthKeyAxios.js
+++ js/axios/api-keys/middlewareAuthKeyAxios.js
@@ -1,58 +1,50 @@
 import express from "express";
-const app = express();
 import axios from "axios";
 import "dotenv/config";
 
 const accessKey = process.env.ACCESS_TOKEN;
 const apiUrl = process.env.production
-  ? "https://api.theauthapi.com"
-  : process.env.TESTING_URL;
+    ? "https://api.theauthapi.com"
+    : process.env.TESTING_URL;
+const app = express();
 
-app.use((req, res, next) => {
-  if (req.headers["x-api-key"]) {
-    try {
-      axios
-        .get(apiUrl + "/api-keys/auth/" + req.headers["x-api-key"], {
+app.use(async (req, res, next) => {
+  const apiKey = req.headers["x-api-key"]
+  if (!apiKey) {
+    return res.status(401).send({
+      message: "No API key, be sure to set it as the 'x-api-key' header",
+    });
+  }
+  try {
+    const {data: authenticatedKey} = await axios
+        .post(`${apiUrl}/api-keys/auth/${apiKey}`, {}, {
           headers: {
             ContentType: "application/json",
             access_key: accessKey,
           },
         })
-        .then((data) => {
-          if (!data.data.key) {
-            return res.status(401).send({ message: "Invalid API key" });
-          }
-          req.user = {
-            accountId: data.data.customAccountId,
-            userId: data.data.customUserId,
-            ...data.data.customMetaData,
-          };
-          next();
-        })
-        .catch(function (error) {
-          if (error.response) {
-            if (error.response.status === 404) {
-              // handle invalid key
-              res.status(401).send({ message: "Error validating API key" });
-            } else if (error.response.status === 429) {
-              // handle api key rate limit error
-              res.status(429).send({ message: "Too many requests try again later" });
-            } else {
-              // handle 401, 403 errors (these should not occur if you did setup your configs correctly
-            }
-          } else if (error.request) {
-            // request was never sent to theauthapi, handle request error here
-          } else {
-            // handle other errors
-          }
-        });
-    } catch (error) {
-      res.status(401).send({ message: "Error validating API key" });
+    req.user = {
+      accountId: authenticatedKey.customAccountId,
+      userId: authenticatedKey.customUserId,
+      ...authenticatedKey.customMetaData,
+    };
+    next();
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status === 404) {
+        // handle invalid key
+        res.status(401).send({message: "Error validating API key"});
+      } else if (error.response.status === 429) {
+        // handle api key rate limit error
+        res.status(429).send({message: "Too many requests try again later"});
+      } else {
+        // handle 401, 403 errors (these should not occur if you did setup your configs correctly
+      }
+    } else if (error.request) {
+      // request was never sent to theauthapi, handle request error here
+    } else {
+      // handle other errors
     }
-  } else {
-    res.status(401).send({
-      message: "No API key, be sure to set it as the 'x-api-key' header",
-    });
   }
 });
 
